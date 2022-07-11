bbac34d146207be715ff80c7604ecb9e0c0d321e
Mahmoudhd134 <100533627+Mahmoudhd134@users.noreply.github.com>
2022-07-11 06:27:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let boxes = gameBord.childNodes;
 let gameOverCover = document.querySelector(".game-over");
 let wins = document.querySelector(".wins");
 let reset = document.querySelector(".reset");
+gameBord.addEventListener("mousedown", (e) => e.preventDefault());
 
 for (let i = 0; i < 9; i++) {
   let div = document.createElement("div");
